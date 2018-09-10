c81e09f90af6f3d11b3ae3132973c3dfc5833f7e
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-09-09 22:24:52 -0400

    Step 2

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
 import * as React from 'react';
-import { Text, View, ScrollView, StyleSheet } from 'react-native';
+import { Text, View, ScrollView, Animated, StyleSheet } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import { Constants } from 'expo';
 import Row from './components/Row';
 import items from './assets/items.json';
 
+const ROW_HEIGHT = 80;
+const PREVIEW_HEIGHT = 200;
+
 class App extends React.Component {
+  scrollRef = React.createRef();
+  scrollY = new Animated.Value(0);
+
+  renderRow = (item, index) => (
+    <Row key={item.id} index={index} scrollY={this.scrollY} {...item} />
+  );
+
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {items.map(item => <Row {...item} />)}
-      </ScrollView>
+      <Animated.ScrollView
+        ref={this.scrollRef}
+        style={styles.container}
+        onLayout={this.onLayout}
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: {
+                contentOffset: { y: this.scrollY },
+              },
+            },
+          ],
+          { useNativeDriver: true }
+        )}
+        snapToInterval={ROW_HEIGHT}
+        decelerationRate="fast"
+        scrollEventThrottle={16}>
+        {items.map(this.renderRow)}
+      </Animated.ScrollView>
     );
   }
 }
@@ -19,6 +45,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f2f2f2',
+    paddingTop: PREVIEW_HEIGHT - ROW_HEIGHT,
   },
 });
 
--- components/Row.js
+++ components/Row.js
@@ -1,41 +1,107 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Animated, Image } from 'react-native';
 
 const ROW_HEIGHT = 80;
+const PREVIEW_HEIGHT = 200;
 
 export default class Row extends React.PureComponent {
 
   static propTypes = {
+    scrollY: PropTypes.any,
+    index: PropTypes.number,
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string,
     backgroundImageUrl: PropTypes.string.isRequired,
     progress: PropTypes.number,
   };
 
-  render() {
-    const {
-      title,
-      subtitle,
-      backgroundImageUrl,
-      progress,
-    } = this.props;
+  inputRange = [
+    ROW_HEIGHT * (this.props.index - 1),
+    ROW_HEIGHT * this.props.index,
+  ];
+  
+
+  renderRow = () => {
+    const transform = [
+      {
+        translateY: this.props.scrollY.interpolate({
+          inputRange: this.inputRange,
+          outputRange: [0, ROW_HEIGHT],
+          extrapolate: 'clamp',
+        }),
+      },
+    ];
 
     return (
-      <View style={styles.row}>
-        <Text style={styles.row__text}>{title}</Text>
-        <View style={styles.row__border} />
+      <Animated.View style={[styles.row, { transform }]}>
+        <Text>{this.props.title}</Text>
+      </Animated.View>
+    );
+  };
+
+  renderPreview = () => {
+    return (
+      <Animated.View
+        style={[styles.preview, {
+        transform: [{
+          translateY: this.props.scrollY.interpolate({
+            inputRange: this.inputRange,
+            outputRange: [0, -PREVIEW_HEIGHT + ROW_HEIGHT],
+            extrapolate: 'clamp',
+          }),
+        }],
+      }]}
+      ><Image
+                source={{ uri: this.props.backgroundImageUrl }}
+                resizeMode="cover"
+                style={StyleSheet.absoluteFill}
+                />
+      </Animated.View>
+    )
+  };
+
+  render() {
+    const { title, subtitle, backgroundImageUrl, progress } = this.props;
+
+    return (
+      <View style={styles.host}>
+        {this.renderPreview()}
+        {this.renderRow()}
+        <View style={styles.border} />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  row: {
+  host: {
     height: ROW_HEIGHT,
     justifyContent: 'center',
     padding: 16,
     backgroundColor: '#ffffff',
+  },
+
+  row: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#ffffff',
+    overflow: 'hidden',
+  },
+
+  preview: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: PREVIEW_HEIGHT,
+    padding: 16,
+    justifyContent: 'center',
+
+    backgroundColor: 'orange',
   },
 
   row__text: {
@@ -44,7 +110,7 @@ const styles = StyleSheet.create({
     color: 'black',
   },
 
-  row__border: {
+  border: {
     position: 'absolute',
     bottom: 0,
     left: 0,
